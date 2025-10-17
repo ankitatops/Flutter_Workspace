7e715f58c217b30c6e49f26d35c638d065b4163b
ankitatops <akshaygondaliya098@gmail.com>
2025-10-17 11:58:18 +0530

    Add files via upload

---
+++ Assignment/module3_assignment/Task24/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Bottom Navigation Bar Demo',
+      home: BottomNavBarExample(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class BottomNavBarExample extends StatefulWidget {
+  @override
+  _BottomNavBarExampleState createState() => _BottomNavBarExampleState();
+}
+
+class _BottomNavBarExampleState extends State<BottomNavBarExample> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('News Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Messages Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Profile Page', style: TextStyle(fontSize: 24))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<String> _titles = ['News', 'Messages', 'Profile'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_titles[_selectedIndex])),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'News'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
