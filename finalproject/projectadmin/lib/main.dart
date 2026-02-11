cfd195a445898560e06abecaf54658f3435c8d3e
ankitatops <akshaygondaliya098@gmail.com>
2026-02-11 10:33:22 +0530

    commit

---
+++ finalproject/projectadmin/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/splashscreen/splash_screen.dart';
+
+
+void main()
+{
+  runApp(MaterialApp(home: SplashScreen(),debugShowCheckedModeBanner: false));
+}
