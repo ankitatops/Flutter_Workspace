87e5137eac1ee6465c4f7d475613aa130ae0216f
ankitatops <akshaygondaliya098@gmail.com>
2026-02-20 09:47:30 +0530

    commit

--- finalproject/digital_society_api/index.php
+++ finalproject/digital_society_api/index.php
@@ -1,50 +1,50 @@
 <?php
-echo "<h2>Ankita projrcts API</h2>";
+echo "<h2>Ankita projects API</h2>";
 
 echo "<ul>";
-echo "<li><a href='add_event.php'>add_event</a></li>";
-echo "<li><a href='add_maintenance.php'>add_maintenance</a></li>";
-echo "<li><a href='add_notice.php'>add_notice</a></li>";
-echo "<li><a href='add_staff.php'>add_staff</a></li>";
-echo "<li><a href='add_visitor.php'>add_visitor</a></li>";
-echo "<li><a href='approve_visitor.php'>approve_visitor</a></li>";
-echo "<li><a href='assign_task.php'>assign_task</a></li>";
-echo "<li><a href='delete_event.php'>delete_event</a></li>";
-echo "<li><a href='delete_maintenance.php'>delete_maintenance</a></li>";
-echo "<li><a href='delete_notice.php'>delete_notice</a></li>";
-echo "<li><a href='delete_staff.php'>delete_staff</a></li>";
-echo "<li><a href='delete_user.php'>delete_user</a></li>";
-echo "<li><a href='get_feedback.php'>get_feedback</a></li>";
-echo "<li><a href='get_problems.php'>get_problems</a></li>";
-echo "<li><a href='update_event.php'>update_event</a></li>";
-echo "<li><a href='update_notice.php'>update_notice</a></li>";
-echo "<li><a href='update_problem_status.php'>update_problem_status</a></li>";
-echo "<li><a href='get_all_users.php'>get_all_users</a></li>";
-echo "<li><a href='get_all_staff.php'>get_all_staff</a></li>";
-echo "<li><a href='get_all_visitors.php'>get_all_visitors</a></li>";
-echo "<li><a href='update_staff_task_status.php'>update_staff_task_status</a></li>";
-echo "<li><a href='update_maintenance.php'>update_maintenance</a></li>";
+echo "<li><a href='ankita/add_event.php'>add_event</a></li>";
+echo "<li><a href='ankita/add_maintenance.php'>add_maintenance</a></li>";
+echo "<li><a href='ankita/add_notice.php'>add_notice</a></li>";
+echo "<li><a href='ankita/add_staff.php'>add_staff</a></li>";
+echo "<li><a href='ankita/add_visitor.php'>add_visitor</a></li>";
+echo "<li><a href='ankita/approve_visitor.php'>approve_visitor</a></li>";
+echo "<li><a href='ankita/assign_task.php'>assign_task</a></li>";
+echo "<li><a href='ankita/delete_event.php'>delete_event</a></li>";
+echo "<li><a href='ankita/delete_maintenance.php'>delete_maintenance</a></li>";
+echo "<li><a href='ankita/delete_notice.php'>delete_notice</a></li>";
+echo "<li><a href='ankita/delete_staff.php'>delete_staff</a></li>";
+echo "<li><a href='ankita/delete_user.php'>delete_user</a></li>";
+echo "<li><a href='ankita/get_feedback.php'>get_feedback</a></li>";
+echo "<li><a href='ankita/get_problems.php'>get_problems</a></li>";
+echo "<li><a href='ankita/update_event.php'>update_event</a></li>";
+echo "<li><a href='ankita/update_notice.php'>update_notice</a></li>";
+echo "<li><a href='ankita/update_problem_status.php'>update_problem_status</a></li>";
+echo "<li><a href='ankita/get_all_users.php'>get_all_users</a></li>";
+echo "<li><a href='ankita/get_all_staff.php'>get_all_staff</a></li>";
+echo "<li><a href='ankita/get_all_visitors.php'>get_all_visitors</a></li>";
+echo "<li><a href='ankita/update_staff_task_status.php'>update_staff_task_status</a></li>";
+echo "<li><a href='ankita/update_maintenance.php'>update_maintenance</a></li>";
 
 echo "</ul>";
 
 echo "<ul>";
-echo "<li><a href='login.php'>login</a></li>";
-echo "<li><a href='register.php'>register</a></li>";
-echo "<li><a href='logout.php'>logout</a></li>";
+echo "<li><a href='ankita/login.php'>login</a></li>";
+echo "<li><a href='ankita/register.php'>register</a></li>";
+echo "<li><a href='ankita/logout.php'>logout</a></li>";
 echo "</ul>";
 
 echo "<ul>";
-echo "<li><a href='add_problem.php'>add_problem</a></li>";
-echo "<li><a href='change_password.php'>change_password</a></li>";
-echo "<li><a href='get_events.php'>get_events</a></li>";
-echo "<li><a href='get_maintenance.php'>get_maintenance</a></li>";
-echo "<li><a href='get_notices.php'>get_notices</a></li>";
-echo "<li><a href='get_staff_tasks.php'>get_staff_tasks</a></li>";
-echo "<li><a href='get_visitors.php'>get_visitors</a></li>";
-echo "<li><a href='send_feedback.php'>send_feedback</a></li>";
-echo "<li><a href='update_profile.php'>update_profile</a></li>";
-echo "<li><a href='upload_profile_image.php'>upload_profile_image</a></li>";
-echo "<li><a href='delete_problem.php'>delete_problem</a></li>";
+echo "<li><a href='ankita/add_problem.php'>add_problem</a></li>";
+echo "<li><a href='ankita/change_password.php'>change_password</a></li>";
+echo "<li><a href='ankita/get_events.php'>get_events</a></li>";
+echo "<li><a href='ankita/get_maintenance.php'>get_maintenance</a></li>";
+echo "<li><a href='ankita/get_notices.php'>get_notices</a></li>";
+echo "<li><a href='ankita/get_staff_tasks.php'>get_staff_tasks</a></li>";
+echo "<li><a href='ankita/get_visitors.php'>get_visitors</a></li>";
+echo "<li><a href='ankita/send_feedback.php'>send_feedback</a></li>";
+echo "<li><a href='ankita/update_profile.php'>update_profile</a></li>";
+echo "<li><a href='ankita/upload_profile_image.php'>upload_profile_image</a></li>";
+echo "<li><a href='ankita/delete_problem.php'>delete_problem</a></li>";
 
 
 echo "</ul>";
